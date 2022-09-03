38f71238c763c30e942fc54f50dc70da8aad5ec3
leye195 <leye195@naver.com>
2022-09-03 10:06:14 +0900

    feat: add Coin, Currency Model

---
+++ src/server/models/Coin.ts
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const coinSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: 'name is required',
+  },
+  KRW: {
+    type: Boolean,
+    default: false,
+  },
+  BTC: {
+    type: Boolean,
+    default: false,
+  },
+  img: {
+    type: String,
+    default: '',
+  },
+  upbit: {
+    type: Boolean,
+    default: false,
+  },
+  binance: {
+    type: Boolean,
+    default: false,
+  },
+  bithumb: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const coinModel = mongoose.model('Coin', coinSchema);
+export default coinModel;
