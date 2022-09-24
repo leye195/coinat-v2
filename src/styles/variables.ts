542c99e9a779e0578142a16170959a3595d284fb
leye195 <leye195@naver.com>
2022-09-25 00:40:00 +0900

    feat: define spacing variables

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -1,14 +1,10 @@
-// breakpoints
+/**
+ * Breakpoint
+ */
 
 export type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 
-export type BreakPoints = {
-  xs: number;
-  sm: number;
-  md: number;
-  lg: number;
-  xl: number;
-};
+export type BreakPoints = Record<Size, number>;
 
 export const breakpoints: BreakPoints = {
   xs: 0,
@@ -17,3 +13,38 @@ export const breakpoints: BreakPoints = {
   lg: 1024,
   xl: 1265,
 };
+
+/**
+ * Spacing
+ */
+
+export type SpacingType =
+  | 'xxs'
+  | 'xs'
+  | 's'
+  | 'm'
+  | 'l'
+  | 'xl'
+  | 'xxl'
+  | 'xxxl';
+export type SpacingNumber = Record<SpacingType, number>;
+export type Spacing = Record<SpacingType, string>;
+
+const spacingNumber: SpacingNumber = {
+  xxs: 4,
+  xs: 8,
+  s: 12,
+  m: 16,
+  l: 20,
+  xl: 24,
+  xxl: 32,
+  xxxl: 40,
+};
+
+export const spacing: Spacing = Object.keys(spacingNumber).reduce(
+  (acc, cur) => {
+    acc[cur as SpacingType] = `${spacingNumber[cur as SpacingType]}px`;
+    return acc;
+  },
+  {} as Spacing,
+);
