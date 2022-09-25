09931f189715d851c6df360f32a661800b9dd2ef
leye195 <leye195@naver.com>
2022-09-25 19:32:44 +0900

    feat: jest, husky setting

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@/styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^@/hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+
+  modulePaths: ['<rootDir>'],
+  moduleDirectories: ['node_modules', 'src'],
+};
+
+module.exports = createJestConfig(customJestConfig);
