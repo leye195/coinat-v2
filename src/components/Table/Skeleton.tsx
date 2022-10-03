21935af404669eaf81cc97e20bd321695c2f2166
leye195 <leye195@naver.com>
2022-10-03 18:38:49 +0900

    style: update table skeleton height

--- src/components/Table/Skeleton.tsx
+++ src/components/Table/Skeleton.tsx
@@ -1,72 +1,87 @@
+import { spacing } from '@/styles/variables';
 import styled from '@emotion/styled';
 import Skeleton from '../Skeleton';
 
 const Container = styled.div`
   width: 100%;
-  margin-top: 1rem;
+  margin-top: ${spacing.s};
+  padding: ${spacing.xs};
+  background-color: ${({ theme }) => theme.color.white};
 `;
 
 const TableSkeleton = () => {
   return (
     <Container>
-      <Skeleton width="100%" height={67.5} borderRadius="8px" />
+      <Skeleton width="100%" height={52} borderRadius="8px" />
       <Skeleton
         width="100%"
-        height={67.5}
+        height={52}
         borderRadius="8px"
         marginTop="0.5rem"
       />
       <Skeleton
         width="100%"
-        height={67.5}
+        height={52}
         borderRadius="8px"
         marginTop="0.5rem"
       />
       <Skeleton
         width="100%"
-        height={67.5}
+        height={52}
         borderRadius="8px"
         marginTop="0.5rem"
       />
       <Skeleton
         width="100%"
-        height={67.5}
+        height={52}
         borderRadius="8px"
         marginTop="0.5rem"
       />
       <Skeleton
         width="100%"
-        height={67.5}
+        height={52}
         borderRadius="8px"
         marginTop="0.5rem"
       />
       <Skeleton
         width="100%"
-        height={67.5}
+        height={52}
         borderRadius="8px"
         marginTop="0.5rem"
       />
       <Skeleton
         width="100%"
-        height={67.5}
+        height={52}
         borderRadius="8px"
         marginTop="0.5rem"
       />
       <Skeleton
         width="100%"
-        height={67.5}
+        height={52}
         borderRadius="8px"
         marginTop="0.5rem"
       />
       <Skeleton
         width="100%"
-        height={67.5}
+        height={52}
         borderRadius="8px"
         marginTop="0.5rem"
       />
       <Skeleton
         width="100%"
-        height={67.5}
+        height={52}
+        borderRadius="8px"
+        marginTop="0.5rem"
+      />
+      <Skeleton
+        width="100%"
+        height={52}
+        borderRadius="8px"
+        marginTop="0.5rem"
+      />
+      <Skeleton
+        width="100%"
+        height={52}
         borderRadius="8px"
         marginTop="0.5rem"
       />
