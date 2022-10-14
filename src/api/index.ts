33bc18bcff7342b16639df3ac990e913a081b21a
leye195 <leye195@naver.com>
2022-10-15 00:30:38 +0900

    feat: add upbit, binance candle api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,14 @@
 import axios, { AxiosResponse } from 'axios';
 import type { Coin } from 'types/Coin';
 import type { Currency } from 'types/Currency';
+import {
+  BinanceCandlesParams,
+  UpbitCandle,
+  UpbitCandlesParams,
+} from 'types/Candle';
+
+const UPBIT_API = `https://api.upbit.com/v1`;
+const BINANCE_API = `https://api.binance.com`;
 
 const api = axios.create({
   headers: {
@@ -9,8 +17,53 @@ const api = axios.create({
   baseURL: '/api/',
 });
 
+const upbitApi = axios.create({
+  baseURL: UPBIT_API,
+});
+
+const binanceApi = axios.create({
+  baseURL: BINANCE_API,
+});
+
 export const getCurrencyInfo = (): Promise<AxiosResponse<Currency>> =>
   api.get('currency');
 
 export const getCoins = (type: 'KRW' | 'BTC'): Promise<AxiosResponse<Coin[]>> =>
   api.get(`coin?type=${type}`);
+
+export const getUpbitCandles = ({
+  market,
+  candleType = 'months',
+  count = 200,
+  minute = 3,
+}: UpbitCandlesParams): Promise<AxiosResponse<UpbitCandle[]>> => {
+  if (candleType === 'minutes')
+    return upbitApi.get(
+      `/candles/minutes/${minute}?market=${market}&count=${count}`,
+    );
+
+  return upbitApi.get(`/candles/${candleType}?market=${market}&count=${count}`);
+};
+
+/**
+ * symbol: xxxBTC
+ * interval:
+ * 1s
+ * 1m
+ * 3m
+ * 5m
+ * 15m
+ * 30m
+ * 1h
+ * 2h
+ * 4h
+ * 6h
+ * 8h
+ * 12h
+ * 1d
+ * 3d
+ * 1w
+ * 1M
+ */
+export const getBinanceCandles = ({ symbol, interval }: BinanceCandlesParams) =>
+  binanceApi.get(`/api/v3/uiKlines?symbol=${symbol}&interval=${interval}`);
---
+++ src/types/Candle.ts
@@ -0,0 +1,49 @@
+export type UpbitCandle = {
+  market: string;
+  candle_date_time_utc: string;
+  candle_date_time_kst: string;
+  opening_price: number;
+  high_price: number;
+  low_price: number;
+  trade_price: number;
+  timestamp: number;
+  candle_acc_trade_price: number;
+  candle_acc_trade_volume: number;
+  prev_closing_price?: number;
+  change_price?: number;
+  change_rate?: number;
+  converted_trade_price?: number;
+  unit?: number;
+  first_day_of_period?: number;
+};
+
+export type UpbitCandlesParams = {
+  market: string;
+  candleType: string;
+  count: number;
+  minute: number;
+};
+
+/*
+1s
+1m
+3m
+5m
+15m
+30m
+1h
+2h
+4h
+6h
+8h
+12h
+1d
+3d
+1w
+1M
+*/
+
+export type BinanceCandlesParams = {
+  symbol: string;
+  interval: string;
+};
