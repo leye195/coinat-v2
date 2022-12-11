193625a5394133e72065b6fca0f45d65514025ee
leye195 <leye195@naver.com>
2022-12-11 13:06:16 +0900

    refactor: move Flex type to type/Style.ts

--- src/styles/mixin.ts
+++ src/styles/mixin.ts
@@ -1,28 +1,6 @@
 import { css } from '@emotion/react';
 import { BreakPoints, breakpointsValue, Size } from '@/styles/variables';
-
-type Flex = {
-  display?: 'flex' | 'inline-flex';
-  direction?: 'row' | 'column' | 'column-reverse' | 'row-reverse';
-  alignItems?:
-    | 'flex-start'
-    | 'flex-end'
-    | 'center'
-    | 'start'
-    | 'end'
-    | 'revert'
-    | 'baseline'
-    | 'inherit';
-  justifyContents?:
-    | 'flex-start'
-    | 'flex-end'
-    | 'center'
-    | 'space-around'
-    | 'space-between'
-    | 'space-evenly'
-    | 'start'
-    | 'end';
-};
+import { Flex } from 'types/Style';
 
 export const flex = ({
   display = 'flex',
--- src/types/Style.ts
+++ src/types/Style.ts
@@ -4,3 +4,26 @@ export type Spacing = {
   left?: string;
   right?: string;
 };
+
+export type Flex = {
+  display?: 'flex' | 'inline-flex';
+  direction?: 'row' | 'column' | 'column-reverse' | 'row-reverse';
+  alignItems?:
+    | 'flex-start'
+    | 'flex-end'
+    | 'center'
+    | 'start'
+    | 'end'
+    | 'revert'
+    | 'baseline'
+    | 'inherit';
+  justifyContents?:
+    | 'flex-start'
+    | 'flex-end'
+    | 'center'
+    | 'space-around'
+    | 'space-between'
+    | 'space-evenly'
+    | 'start'
+    | 'end';
+};
