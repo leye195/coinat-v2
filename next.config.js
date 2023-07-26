96733ec68b88872cd624d8af43fb275b8e32a4c8
leye195 <leye195@naver.com>
2023-07-26 21:04:19 +0900

    chore: add @svgr/webpack & update webpack setting

--- next.config.js
+++ next.config.js
@@ -2,6 +2,35 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg'),
+    );
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              svgo: false,
+            },
+          },
+        ],
+      },
+    );
+
+    return config;
+  },
   compiler: {
     emotion: true,
     removeConsole: process.env.NEXT_PUBLIC_ENV === 'production',
