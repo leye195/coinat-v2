9d7b5719f41e9d0c202734a8e8ae1b0392187d90
leye195 <leye195@naver.com>
2023-08-14 18:55:02 +0900

    feat: remove constrainedWidth

--- src/components/Chart/Candle.tsx
+++ src/components/Chart/Candle.tsx
@@ -1,8 +1,8 @@
+import { useMemo } from 'react';
 import { AxisRight } from '@visx/axis';
 import { Group } from '@visx/group';
 import { scaleBand, scaleLinear } from '@visx/scale';
 import { BoxPlot } from '@visx/stats';
-import { useMemo } from 'react';
 
 type Props<T> = {
   width: number;
@@ -20,7 +20,6 @@ const margin = {
 const x = (d: any) => d.date;
 const open = (d: any) => (d.open >= d.close ? d.open : d.close);
 const close = (d: any) => (d.open < d.close ? d.open : d.close);
-const median = (d: any) => (d.open + d.close) / 2;
 
 const Candle = ({ width, height, data }: Props<any>) => {
   const xMax = width - 40;
@@ -47,30 +46,28 @@ const Candle = ({ width, height, data }: Props<any>) => {
       }),
     [data, yMax],
   );
-  const boxWidth = xScale.bandwidth();
-  const constrainedWidth = Math.min(0, boxWidth);
 
   return (
     <svg width={width} height={height}>
-      <Group top={-40}>
+      <Group>
         {data.map((d, idx) => (
           <g key={`box-plot-${idx}`}>
             <BoxPlot
               valueScale={yScale}
-              left={xScale(x(d))! + 0.4 * constrainedWidth}
+              left={xScale(x(d))}
               min={d.low}
               max={d.high}
               firstQuartile={open(d)}
-              median={median(d)}
               thirdQuartile={close(d)}
               fill={d.open > d.close ? '#1261c4' : '#c84a31'}
               stroke={d.open > d.close ? '#1261c4' : '#c84a31'}
-              boxWidth={8}
+              strokeWidth={1}
+              boxWidth={xScale.bandwidth()}
             />
           </g>
         ))}
       </Group>
-      <AxisRight scale={yScale} left={xMax} top={0} />
+      <AxisRight scale={yScale} left={xMax} />
     </svg>
   );
 };
