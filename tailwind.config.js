b5797dbcce0a2fe92af24d3c17d9dec2d389e770
leye195 <leye195@naver.com>
2024-06-22 13:23:53 +0900

    fix: build error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-import type { Config } from 'tailwindcss';
 const { colors, twTypographyMap } = require('ownui-system');
 const plugin = require('tailwindcss/plugin');
 
-const config: Config = {
+const config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
