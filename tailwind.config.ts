aee00274fd9ffe5ef9a8a53eff4cb081a3de9f2a
leye195 <leye195@naver.com>
2024-06-22 13:22:56 +0900

    chore: add autoprefixer&postcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,21 @@
 import type { Config } from 'tailwindcss';
+const { colors, twTypographyMap } = require('ownui-system');
+const plugin = require('tailwindcss/plugin');
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './node_modules/ownui-system/dist/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {},
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      // Add your custom styles here
+      addUtilities(twTypographyMap, ['responsive', 'hover']);
+    }),
+  ],
 };
 export default config;
