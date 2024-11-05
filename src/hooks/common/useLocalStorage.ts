1d7a6f70b598b2c760c3801439c71f0463f8b3ff
leye195 <leye195@naver.com>
2024-11-05 23:29:07 +0900

    fix: import error on useLocalStorage

--- src/hooks/common/useLocalStorage.ts
+++ src/hooks/common/useLocalStorage.ts
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import useIsomorphicLayoutEffect from '@/hooks/useIsomorphicLayoutEffect';
+import { useIsomorphicLayoutEffect } from '@/hooks';
 import { getLocalStorageData, setLocalStorageData } from '@/lib/storage';
 
 type UseLocalStorageProps = {
