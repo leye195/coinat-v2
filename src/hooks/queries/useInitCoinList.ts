41fd4d168579c580f14d2766b7234e8e84050f9d
leye195 <leye195@naver.com>
2024-11-22 00:33:20 +0900

    feat: update useInitCoinList

--- src/hooks/queries/useInitCoinList.ts
+++ src/hooks/queries/useInitCoinList.ts
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { initSocket } from '@/lib/socket';
+import { getLocalStorageData, setLocalStorageData } from '@/lib/storage';
 import { btcCoinListState, krCoinListState } from '@/store/coin';
 
 const useInitCoinList = () => {
@@ -9,6 +10,18 @@ const useInitCoinList = () => {
   const setBtcCoinList = useSetRecoilState(btcCoinListState);
 
   useEffect(() => {
+    const localData = getLocalStorageData('coins');
+
+    // TODO: 저장시간 비교,
+    if (localData) {
+      const { krw, btc } = localData;
+      setKrwCoinList({
+        isLoading: false,
+        data: krw,
+      });
+      setBtcCoinList({ isLoading: false, data: btc });
+    }
+
     workerRef.current = new Worker(
       new URL('/public/workers/workers/crypto.worker.js', import.meta.url),
     );
@@ -24,6 +37,13 @@ const useInitCoinList = () => {
       });
       setBtcCoinList({ isLoading: false, data: btc });
       initSocket([...krw, ...btc]);
+      setLocalStorageData(
+        'coins',
+        JSON.stringify({
+          krw,
+          btc,
+        }),
+      );
     };
     workerRef.current.onerror = (error) => {
       console.error('Worker error:', error.message);
