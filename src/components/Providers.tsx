e93b6085e4c3c42dfdb07a6968487ab01a811d76
leye195 <leye195@naver.com>
2024-11-23 15:35:42 +0900

    refactor: remove themeProvider

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,22 +1,16 @@
 'use client';
 
 import { PropsWithChildren, useState } from 'react';
-import { ThemeProvider as EmotionThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RecoilRoot } from 'recoil';
-import { globalTheme } from '@/styles/theme';
 
 function Providers({ children }: PropsWithChildren) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <EmotionThemeProvider theme={globalTheme}>
-      <RecoilRoot>
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </RecoilRoot>
-    </EmotionThemeProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </RecoilRoot>
   );
 }
 
