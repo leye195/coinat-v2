5c69183071cfbd7becb19264bf1ac3ab96d5fe10
leye195 <leye195@naver.com>
2025-05-03 23:39:37 +0900

    refactor: remove dulicated logics

--- src/hooks/queries/useInitCoinList.ts
+++ src/hooks/queries/useInitCoinList.ts
@@ -1,9 +1,7 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { useSetRecoilState } from 'recoil';
-import { initSocket } from '@/lib/socket';
-import { getLocalStorageData, setLocalStorageData } from '@/lib/storage';
 import {
   btcCoinListState,
   krCoinListState,
@@ -12,25 +10,16 @@ import {
 import { Coin } from '@/types/Coin';
 
 type UseInitCointListProps = {
-  workerEnabled?: boolean;
   initialData: Record<'krw' | 'btc' | 'usdt', Coin[]>;
 };
 
-const useInitCoinList = ({
-  workerEnabled = true,
-  initialData,
-}: UseInitCointListProps) => {
-  const workerRef = useRef<Worker | null>(null);
+const useInitCoinList = ({ initialData }: UseInitCointListProps) => {
   const setKrwCoinList = useSetRecoilState(krCoinListState);
   const setBtcCoinList = useSetRecoilState(btcCoinListState);
   const setUsdtCoinList = useSetRecoilState(usdtCoinListState);
 
   useEffect(() => {
-    if (
-      workerEnabled ||
-      initialData.krw.length === 0 ||
-      initialData.btc.length === 0
-    ) {
+    if (initialData.krw.length === 0 || initialData.btc.length === 0) {
       return;
     }
 
@@ -42,62 +31,7 @@ const useInitCoinList = ({
     });
     setBtcCoinList({ isLoading: false, data: btc });
     setUsdtCoinList({ isLoading: false, data: usdt });
-
-    initSocket([...krw, ...btc, ...usdt]);
-  }, [
-    workerEnabled,
-    initialData,
-    setBtcCoinList,
-    setKrwCoinList,
-    setUsdtCoinList,
-  ]);
-
-  useEffect(() => {
-    if (!workerEnabled) return;
-
-    const localData = getLocalStorageData('coins');
-
-    if (localData) {
-      const { krw, btc, usdt } = localData;
-      setKrwCoinList({
-        isLoading: false,
-        data: krw,
-      });
-      setBtcCoinList({ isLoading: false, data: btc });
-      setUsdtCoinList({ isLoading: false, data: usdt });
-    }
-
-    workerRef.current = new Worker(
-      new URL('/public/workers/workers/crypto.worker.js', import.meta.url),
-    );
-
-    if (workerRef.current) workerRef.current.postMessage('start');
-
-    workerRef.current.onmessage = (event) => {
-      const { krw, btc } = event.data;
-
-      setKrwCoinList({
-        isLoading: false,
-        data: krw,
-      });
-      setBtcCoinList({ isLoading: false, data: btc });
-      initSocket([...krw, ...btc]);
-      setLocalStorageData(
-        'coins',
-        JSON.stringify({
-          krw,
-          btc,
-        }),
-      );
-    };
-    workerRef.current.onerror = (error) => {
-      console.error('Worker error:', error.message);
-    };
-
-    return () => {
-      workerRef.current?.terminate();
-    };
-  }, [workerEnabled, setBtcCoinList, setKrwCoinList, setUsdtCoinList]);
+  }, [initialData, setBtcCoinList, setKrwCoinList, setUsdtCoinList]);
 };
 
 export default useInitCoinList;
