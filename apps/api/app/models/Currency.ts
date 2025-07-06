ae770e751ada472032c4df58e02c97f69ee71f83
leye195 <leye195@naver.com>
2025-07-06 19:34:27 +0900

    feat: Coin, Currency Model 생성

---
+++ apps/api/app/models/Currency.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const currencySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: 'name is required',
+    },
+    value: {
+      type: Number,
+      default: 0,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  { timestamps: true },
+);
+const currencyModel =
+  mongoose.models.Currency || mongoose.model('Currency', currencySchema);
+export default currencyModel;
